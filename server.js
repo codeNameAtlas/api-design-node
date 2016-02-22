aa837559a58b85ed9e36b666b5850da130168555
codeNameAtlas <anavarrete255@gmail.com>
2016-02-21 16:28:40 -0800

    set up a basic server with express and serve a static index.html

--- server.js
+++ server.js
@@ -2,4 +2,24 @@
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
 
+
+var express = require('express')
+
+var app = express();
+
 var jsonData = {count: 12, message: 'hey'};
+
+app.get('/', function(req, res) {
+  //render the index.html file
+  res.sendfile('index.html');
+});
+
+app.get('/data', function(req, res) {
+  var data = JSON.stringify(jsonData)
+  res.send(data);
+  //alternatively
+  //res.json(jsonData);
+})
+
+app.listen(8080);
+
